1f07446bea6ec294d92d369cc41702b5cf978390
mDafonte <md.dafonte@gmail.com>
2022-06-10 10:24:55 +0200

    onlyCode

--- data/Simu_FSE/GA_26w/low_motion/AX/no_shift/visualize.m
+++
@@ -1,9 +0,0 @@
-p=load(["Fetal_Brain_FSE_Images.mat"],"FSE_Images").FSE_Images;
-for i=1:50
-    figure(1)
-    imagesc(real(p(:,:,i)))
-    colormap("gray")
-    colorbar
-    axis image
-    pause(1)
-end
